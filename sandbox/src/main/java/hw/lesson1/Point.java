f203d3d5bbf39213921c24f970b6253a42932e14
w0lfi515 <lida-zavarzina@yandex.ru>
2018-07-04 23:20:58 +0300

    reformat

--- sandbox/src/main/java/hw/lesson1/Point.java
+++ sandbox/src/main/java/hw/lesson1/Point.java
@@ -4,14 +4,14 @@ public class Point {
     public double x;
     public double y;
 
-    public Point (double x, double y){
+    public Point(double x, double y) {
         this.x = x;
         this.y = y;
     }
 
     public double distance(Point p1) {
         double dist;
-        dist = Math.sqrt( (p1.x - this.x)*(p1.x - this.x)  +  (p1.y - this.y)* (p1.y - this.y));
+        dist = Math.sqrt((p1.x - this.x) * (p1.x - this.x) + (p1.y - this.y) * (p1.y - this.y));
         return dist;
     }
 }
